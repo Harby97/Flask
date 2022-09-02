6794118af8386b0441cb8a2b194829b0a0331811
Harby97 <ingharby@gmail.com>
2022-09-02 10:26:10 -0500

    se agregaron validadores a los campos de entrada y se cambió el método a post. Ahora se muestra el usuario en pantalla luego del envío de los datos de form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request , make_response, redirect, render_template, session
+from flask import Flask, request , make_response, redirect, render_template, session, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
@@ -30,19 +30,27 @@ def index():
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET','POST'])
 def hello():
     information = ['php', 'python', 'Java']
     user_ip = session.get('user_ip')
     # user_ip = request.cookies.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
     data = {                #contexto
         'title': 'Index_Flask',
         'courses': information,
         'number_of_courses': len(information),
         'user_ip': user_ip,
-        'login_form': login_form
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        return redirect(url_for('index'))
+
     return render_template('base.html', data=data)
 
 
